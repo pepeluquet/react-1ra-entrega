595e33ff3ac8e30c0eaf927879b8a24cd6542ebb
Christian Luquet <pepeluquet@gmail.com>
2025-06-19 15:24:30 -0300

    agregando Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
+import Counter from './components/Counter'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             </main>
             <section className="nucleo">
                 <ItemListContainer titulo="¡Bienvenidos! Tienda InaYoga" />
+                <Counter />
             </section>
             <footer className="final">
                 {/* Pie de página */}
