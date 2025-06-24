8d104d090295f8607929344b2e3342b133e099a1
Christian Luquet <pepeluquet@gmail.com>
2025-06-24 15:50:05 -0300

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,20 @@ import './App.css'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import Counter from './components/Counter'
+import { BrowserRouter, Routes, Route } from "react-router"
 
 function App() {
     return (
-        <div className="grid-container">
+        <BrowserRouter>
             <NavBar />
-            <main className="portada">
-                {/* Aquí puedes poner un banner o portada */}
-            </main>
-            <section className="nucleo">
-                <ItemListContainer titulo="¡Bienvenidos! Tienda InaYoga" />
-                <Counter />
-            </section>
-            <footer className="final">
-                {/* Pie de página */}
-            </footer>
-        </div>
+            <Routes>
+                <Route path="/" element={<section className="nucleo">
+                                            <ItemListContainer titulo="¡Bienvenidos! Tienda InaYoga" />
+                                            <Counter />
+                                        </section>} />
+            </Routes>
+
+        </BrowserRouter>
     )
 }
 
