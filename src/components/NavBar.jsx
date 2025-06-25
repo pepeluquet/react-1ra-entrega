5ed88add392c0ea117a393a7f547baebc4169672
Christian Luquet <pepeluquet@hotmail.com>
2025-06-24 23:44:44 -0300

    NavBar categorias de la api

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,18 @@
 import './style.css'
 import CartWidget from './CartWidget'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router'
 
 function NavBar() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/products/category-list')
+            .then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error('Error fetching categories:', error))
+    }, [])
+
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
@@ -14,18 +25,18 @@ function NavBar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="productosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Productos
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="productosDropdown">
-                                <li><a className="dropdown-item" href="#">Mats</a></li>
-                                <li><a className="dropdown-item" href="#">Rodillos</a></li>
-                                <li><a className="dropdown-item" href="#">Cintas de elongar</a></li>
-                                <li><a className="dropdown-item" href="#">Pelotas</a></li>
-                                <li><a className="dropdown-item" href="#">Tacos de goma</a></li>
+                                {categories.map(cat => (
+                                    <li key={cat}>
+                                        <Link className="dropdown-item" to={`/categoria/${cat}`}>{cat}</Link>
+                                    </li>
+                                ))}
                             </ul>
                         </li>
                     </ul>
